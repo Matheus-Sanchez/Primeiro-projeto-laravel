a9e8212708bb194d34d1447728ed0c4900aa600c
Matheus Duda <mathbloodfire@gmail.com>
2023-09-13 17:08:30 -0300

    Initial commit

---
+++ app/Http/Controllers/aboutController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class aboutController extends Controller
+{
+    //
+    public function index()
+    {
+        // Você pode passar quaisquer dados necessários para a view aqui
+        $dados = [
+            'title' => 'Sobre o Projeto',
+            'description' => 'Este é um projeto Laravel incrível!'
+        ];
+
+        return view('about', $dados);
+    }
+}
